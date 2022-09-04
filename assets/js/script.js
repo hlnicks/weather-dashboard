429881440318a0e394c40253c953c1534c131978
hlnicks <hlnicks@icloud.com>
2022-09-04 16:42:48 -0400

    added more to HTML layout and added pseudocode to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// OpenWeather One Call: https://openweathermap.org/api/one-call-api
+
+// functions needed:
+
+// submithandler
+// fetches data from api
+// renders weather
+// renders 5-day forecast
+// saves city input to localstorage
+// click even
