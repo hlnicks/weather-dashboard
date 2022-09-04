ad879ab597c0bb162e91760e14e7be9245dfd2ff
hlnicks <hlnicks@icloud.com>
2022-09-04 17:25:52 -0400

    started functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,39 @@ var listCities = document.querySelector("#city-list");
 var currentWeather = document.querySelector("#city-weather");
 var futureForecast = document.querySelector("#five-day");
 
-// functions needed:
+// functions
 
-// submithandler
+// handles user submission
+var formHandler = function(event) {
+
+};
+
 // fetches data from api
+var fetchWeather = function() {
+
+};
+
 // renders weather
+var displayWeather = function() {
+
+};
+
 // renders 5-day forecast
+var displayFuture = function() {
+
+};
+
 // saves city input to localstorage
+var saveCity = function () {
+
+};
+
+// loads cities from localStorage
+var loadCity = function(){
+
+};
+
 // click event
+
+
+
